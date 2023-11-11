7c1b3d3b954156d557c3f070454170ced774314e
HiddenVault <144142448+HiddenVault@users.noreply.github.com>
2023-11-11 17:29:50 +0100

    Chargement des dataframes en mémoireUtilisation d'une boucle for dans le but d'automatise le merge

--- src/fusion.dataframes.py
+++ src/fusion.dataframes.py
@@ -1,34 +1,22 @@
-import moduleOS
-import moduleCSV
-import moduleDownload
-import moduleOS
-import moduleDataframe
-import modulePreparationHTML
-import time
-import pandas as pd
-import numpy as np
 import os
+import pandas as pd
 
 # Dictionnaire avec les noms des fichiers, leurs emplacements, leur type de séparateur et le nombre de lignes
 files_dict = {
-    'P_title.episode.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_title.principals.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_title.akas.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_complementaire_tmdb_full.csv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_title.ratings.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_title.crew.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_title.basics.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
-    'P_name.basics.tsv.csv_explore.csv' :('./data/preparation', ',', -1, 10, 10, 10),
+    'P_title.basics.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_name.basics.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_title.principals.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_title.crew.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_complementaire_tmdb_full.csv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_title.akas.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+    'P_title.ratings.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+#    'P_title.episode.tsv.csv_explore.csv': ('./data/preparation', ',', -1)
 }
 
 # Préfixe pour le fichier CSV
 file_prefix = 'F_'
 
-# Début du chronomètre
-import time
-start_time = time.time()
-
-dataframes = {}  # Dictionnaire pour stocker les DataFrames
+dataframes = {}  # Dictionnaire pour stocker les DataFrames en mémoire
 
 for file_name, (directory, separator, lines, *_) in files_dict.items():
     file_path = os.path.join(directory, file_name)
@@ -42,17 +30,17 @@ for file_name, (directory, separator, lines, *_) in files_dict.items():
     except Exception as e:
         print(f"Erreur lors de la lecture de {file_name}: {e}")
 
-print(dataframes)
+# Fusion des DataFrames en utilisant la colonne tconst
+merged_data = None  # Initialisation dictionnaire à zéro.
 
-merged_df = pd.merge(dataframes['P_title.basics.tsv.csv_explore.csv'], dataframes['P_title.akas.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_complementaire_tmdb_full.csv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_title.ratings.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_title.crew.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_title.principals.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_name.basics.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='inner')
-merged_df = pd.merge(merged_df, dataframes['P_title.episode.tsv.csv_explore.csv'], left_on='tconst', right_on='tconst', how='left')
-
-print(merged_df)
+for df_name, df in dataframes.items():
+    if merged_data is None:
+        merged_data = df
+        print(f"Merging {df_name}...")        
+    else:
+        print(f"Merging {df_name}...")
+        merged_data = merged_data.merge(df, on='tconst', how='inner')
+        print(merged_data.head())
 
 # Fichier de sortie
 output_filename = f"{file_prefix}merged_data.csv"
@@ -60,7 +48,7 @@ output_filename = f"{file_prefix}merged_data.csv"
 # Chemin complet du fichier de sortie en utilisant os.path.join
 output_file_path = os.path.join(directory, output_filename)
 
-# Enregistrement du dataFrame dans un fichier CSV
-merged_df.to_csv(output_file_path, index=False)
+# Enregistrement du DataFrame fusionné dans un fichier CSV
+merged_data.to_csv(output_file_path, index=False)
 
 print(f"Le fichier CSV a été enregistré sous : {output_file_path}")
