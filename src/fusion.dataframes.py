e2e7db7e6a1e4d1fe6706e19236cbf111195dfc6
HiddenVault <144142448+HiddenVault@users.noreply.github.com>
2023-11-11 17:33:42 +0100

    Optimisation : Les dataframes sont chargés au fur et à mesure de leur fusion.
    La consommation de mémoire est moindre.

--- src/fusion.dataframes.py
+++ src/fusion.dataframes.py
@@ -4,20 +4,22 @@ import pandas as pd
 # Dictionnaire avec les noms des fichiers, leurs emplacements, leur type de séparateur et le nombre de lignes
 files_dict = {
     'P_title.basics.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
+#    'F_merged_data_nconst.csv': ('./data/preparation', ',', -1),
     'P_name.basics.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
     'P_title.principals.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
     'P_title.crew.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
     'P_complementaire_tmdb_full.csv.csv_explore.csv': ('./data/preparation', ',', -1),
     'P_title.akas.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
     'P_title.ratings.tsv.csv_explore.csv': ('./data/preparation', ',', -1),
-#    'P_title.episode.tsv.csv_explore.csv': ('./data/preparation', ',', -1)
+    # 'P_title.episode.tsv.csv_explore.csv': ('./data/preparation', ',', -1)
 }
 
 # Préfixe pour le fichier CSV
 file_prefix = 'F_'
 
-dataframes = {}  # Dictionnaire pour stocker les DataFrames en mémoire
+merged_data = None  # Initialisation du DataFrame fusionné à zéro.
 
+# Fusion des DataFrames en utilisant la colonne tconst
 for file_name, (directory, separator, lines, *_) in files_dict.items():
     file_path = os.path.join(directory, file_name)
     try:
@@ -26,21 +28,16 @@ for file_name, (directory, separator, lines, *_) in files_dict.items():
                 df = pd.read_csv(file_path, sep=separator)
             else:
                 df = pd.read_csv(file_path, sep=separator, nrows=lines)
-            dataframes[file_name] = df
+            
+            if merged_data is None:
+                merged_data = df
+                print(f"Merging {file_name}...")
+            else:
+                print(f"Merging {file_name}...")
+                merged_data = merged_data.merge(df, on='tconst', how='inner')
+                print(merged_data.head())
     except Exception as e:
         print(f"Erreur lors de la lecture de {file_name}: {e}")
-
-# Fusion des DataFrames en utilisant la colonne tconst
-merged_data = None  # Initialisation dictionnaire à zéro.
-
-for df_name, df in dataframes.items():
-    if merged_data is None:
-        merged_data = df
-        print(f"Merging {df_name}...")        
-    else:
-        print(f"Merging {df_name}...")
-        merged_data = merged_data.merge(df, on='tconst', how='inner')
-        print(merged_data.head())
 
 # Fichier de sortie
 output_filename = f"{file_prefix}merged_data.csv"
@@ -49,6 +46,7 @@ output_filename = f"{file_prefix}merged_data.csv"
 output_file_path = os.path.join(directory, output_filename)
 
 # Enregistrement du DataFrame fusionné dans un fichier CSV
+print(f"Le fichier CSV est en cours de génération...")
 merged_data.to_csv(output_file_path, index=False)
 
 print(f"Le fichier CSV a été enregistré sous : {output_file_path}")
