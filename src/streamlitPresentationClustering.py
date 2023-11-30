71a82b604fd143da566710ec52bcedaddadaba84
HiddenVault <144142448+HiddenVault@users.noreply.github.com>
2023-11-30 17:40:39 +0100

    streamlitPresentationClustering.py : Modification de la mise en page

--- src/streamlitPresentationClustering.py
+++ src/streamlitPresentationClustering.py
@@ -3,120 +3,90 @@ import streamlit as st
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import warnings
-from PIL import Image
 
-# Ignorer les avertissements
+# Les avertissements sont ignorés
 warnings.filterwarnings("ignore")
 
-# Charger le DataFrame à partir du fichier CSV
-df = pd.read_csv('./data/preparation/ML_F2_merged_data_v3.csv', sep=',', encoding='UTF-8', low_memory=False)
+# Chargement du dataframe à partir du fichier CSV
+df = pd.read_csv('ML_F2_merged_data_v3.csv', sep=',', encoding='UTF-8', low_memory=False)
 
-# Sélection des colonnes numériques du DataFrame
+# Sélection des colonnes numériques du dataframe
 df_numeric = df.select_dtypes(include=['float64', 'int64'])
 
 # Normalisation des données numériques
 scaler = StandardScaler()
 df_numeric_normalized = scaler.fit_transform(df_numeric)
 
-# Utiliser KMeans pour regrouper les films en clusters
+# Utilisation de KMEANS pour regrouper les films en clusters
 n_clusters = 9
 kmeans = KMeans(n_clusters=n_clusters, random_state=42)
 df['cluster'] = kmeans.fit_predict(df_numeric_normalized)
 
 # Menu Streamlit
-# Affichage de la bannière
-image_url= 'http://hiddenvault.fr/P2_Systeme_de_recommandation_de_films/banner.png'
-code_html = f"<img src='{image_url}' width='100%'/>"
-st.markdown(code_html, unsafe_allow_html = True)
+# Lecture du css à appliquer à la page
+def lire_css(fichier):
+    with open(fichier, 'r', encoding='utf-8') as f:
+        return f.read()
+contenu_html = lire_css('./pages/st.css')
 
-st.title("Movie recommendations")
+# Application de la css
+st.markdown(contenu_html, unsafe_allow_html = True)
 
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: #000000; /* Noir */
-            color: #FFFFFF; /* Blanc */
-            margin: 20px !important;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.header("Notre recommandation...")
 
-# Saisie du nom du film
-user_input = st.text_input("Enter movie name :")
+# Tri des valeurs uniques par ordre alphabétique
+unique_movie_titles = sorted(df['TI_primaryTitle'].unique())
 
-if user_input:
-    # Recherche et recommandation des films
-    matching_movies = df[df['TI_primaryTitle'].str.contains(user_input, case=False, na=False)]
+# Utilisation de la liste triée dans la liste déroulante
+selected_movie_title = st.selectbox("Saisir un nom de film :", unique_movie_titles, key='movie_selectbox', index=None)
 
-    if matching_movies.empty:
-        st.warning(f"Aucun film trouvé avec la correspondance '{user_input}'. Veuillez réessayer.")
-    else:
-        # Affichage des films correspondants dans une liste déroulante
-        selected_movie = st.selectbox("Select movie :", matching_movies['TI_primaryTitle'])
+    # Sélection du film
+if  selected_movie_title:
+    # Filtrage du dataframe en fonction du film sélectionné
+    selected_movie_info = df[df['TI_primaryTitle'] == selected_movie_title].iloc[0]
 
-        # Récupération des informations sur le film sélectionné
-        selected_movie_info = matching_movies[matching_movies['TI_primaryTitle'] == selected_movie].iloc[0]
+    # Normalisation des données du film sélectionné
+    movie_data_normalized = scaler.transform(selected_movie_info[df_numeric.columns].values.reshape(1, -1))
 
-        # Normalisation des données du film sélectionné
-        movie_data_normalized = scaler.transform(selected_movie_info[df_numeric.columns].values.reshape(1, -1))
+    # Prédiction du cluster du film sélectionné
+    selected_movie_cluster = kmeans.predict(movie_data_normalized)
 
-        # Prédiction du cluster du film sélectionné
-        selected_movie_cluster = kmeans.predict(movie_data_normalized)
+    # Filtrage des films du même cluster
+    cluster_movies = df[df['cluster'] == selected_movie_cluster[0]]
+    cluster_movies = cluster_movies[cluster_movies['TI_primaryTitle'] != selected_movie_info['TI_primaryTitle']]
 
-        # Filtrage des films du même cluster
-        cluster_movies = df[df['cluster'] == selected_movie_cluster[0]]
-        cluster_movies = cluster_movies[cluster_movies['TI_primaryTitle'] != selected_movie_info['TI_primaryTitle']]
+    # Sélection des films recommandés
+    recommended_movies = cluster_movies.loc[:, ['TI_primaryTitle', 'TI_startYear', 'RA_averageRating', 'RA_numVotes', 'TI_overview', 'TI_poster_path']]
 
-        # Sélection des films recommandés
-        recommended_movies = cluster_movies.loc[:, ['TI_primaryTitle', 'TI_startYear', 'TI_budget', 'TI_revenue', 'RA_averageRating', 'TI_runtimeMinutes', 'TI_overview', 'RA_numVotes', 'TI_region', 'TI_language','TI_poster_path']].head(5)
+    # Affichage des informations du film sélectionné
+    st.header(f"Film sélectionné :")
+    st.subheader(f"{selected_movie_info['TI_primaryTitle']}")
 
-        # Affichage des informations du film sélectionné
-        st.header("Selected Movie:")
+    # Présention sur deux colonnes
+    col1, col2 = st.columns([1, 2])
+
+    col1.image(selected_movie_info['TI_poster_path'], width=400, use_column_width=True)
+
+    with col2:
+        st.write(f"Date de sortie : {round(selected_movie_info['TI_startYear'])}")
+        st.write(f"Note moyenne : {selected_movie_info['RA_averageRating']}")
+        st.write(f"Votes : {round(selected_movie_info['RA_numVotes'])}")
+        st.write(f"Résumé :<br>{selected_movie_info['TI_overview']}", unsafe_allow_html=True)
+
+    st.header("Films recommandés :")
+
+    # Affichage des recommandations
+    for _, row in recommended_movies.head(5).iterrows():
+        st.subheader(f"{row['TI_primaryTitle']}")
+
+        col1, col2 = st.columns([1, 2])
+
+        col1.image(row['TI_poster_path'], width=400, use_column_width=True)
+
+        with col2:
+            st.write(f"Date de sortie : {round(row['TI_startYear'])}")
+            st.write(f"Note moyenne : {row['RA_averageRating']}")
+            st.write(f"Votes : {round(row['RA_numVotes'])}")
+            st.write(f"Résumé :<br>{row['TI_overview']}", unsafe_allow_html=True)
         
-        col1, col2 = st.columns([1, 3])
-        col1.image(selected_movie_info['TI_poster_path'], width=400, use_column_width=True)
-
-        title_container = col2.empty()
-        release_container = col2.empty()
-        ratings_container = col2.empty()
-        votes_container = col2.empty()
-
-        title_container.text_input("Title :", key="title_selected", value=selected_movie_info['TI_primaryTitle'])
-        release_container.text_input("Release :", key="release_selected", value=selected_movie_info['TI_startYear'])
-        ratings_container.text_input("Average Ratings :", key="ratings_selected", value=selected_movie_info['RA_averageRating'])
-        votes_container.text_input("Votes :", key="votes_selected", value=selected_movie_info['RA_numVotes'])
-
-        st.write(f"Summary :")
-        st.write(f"{selected_movie_info['TI_overview']}")
-
-        st.header("Recommended movies :")
-
-        # Affichage des recommandations
-        for index, row in recommended_movies.iterrows():
-            movie_title = row['TI_primaryTitle']
-            poster_path = row['TI_poster_path']
-            overview = row['TI_overview']
-            start_year = row['TI_startYear']
-            average_rating = row['RA_averageRating']
-            num_votes = row['RA_numVotes']
-
-            col1, col2 = st.columns([1, 3])
-            col1.image(poster_path, width=400, use_column_width=True)
-
-            title_container = col2.empty()
-            release_container = col2.empty()
-            ratings_container = col2.empty()
-            votes_container = col2.empty()
-
-            title_container.text_input("Title :", key=f"title_{index}", value=movie_title)
-            release_container.text_input("Release :", key=f"release_{index}", value=start_year)
-            ratings_container.text_input("Average Ratings :", key=f"ratings_{index}", value=average_rating)
-            votes_container.text_input("Votes :", key=f"votes_{index}", value=num_votes)
-
-            st.write(f"Summary :")
-            st.write(f"{overview}")
-
-            st.markdown("<hr>", unsafe_allow_html=True)
+        st.markdown("---")
