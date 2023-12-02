ce991c78d97de09448246dd5e9e3dbae1ed6abfb
Marc-B-WCS-Data-092023 <144699048+Marc-B-WCS-Data-092023@users.noreply.github.com>
2023-12-02 02:18:24 +0100

    streamlit final

--- src/streamlit/Accueil.py
+++ src/streamlit/Accueil.py
@@ -4,9 +4,31 @@ import streamlit as st
 def lire_css(fichier):
     with open(fichier, 'r', encoding='utf-8') as f:
         return f.read()
-    
+
+# Use the full page instead of a narrow central column
+st.set_page_config(layout="centered")
 # Lire le fichier de styles perso
 contenu_html = lire_css('pages/st.css')
 st.markdown(contenu_html, unsafe_allow_html = True)
 
-st.header('Choisir un type d\'algorithme de ML')
+st.title('Choisir une méthode de recommandation...')
+
+st.markdown('''
+            ## **Caractéristiques utilisées :**
+            
+            Pour les 2 méthodes utilisés, les caractéristiques prises en compte dans le modèle sont les suivantes :
+            L'année de sortie, le budget, la recette, la note moyenne, le nombre de votes, la durée et le genre
+            ''')
+
+st.markdown('''
+            ## **Méthode du Clustering :**
+            
+            Cet algorithme de Machine Learning permet sur compartimenter les films par groupe de caractéristiques similaires.
+            Dans le cas présent les films sont divisés en 3 groupes.
+            ''')
+
+st.markdown('''
+            ## **Méthode du KNN :**
+            
+            Cet algorithme de Machine Learning permet de retrouver les 5 films les plus proches en combinant toutes les caractéristiques d'un film avec ces plus proches voisins
+            ''')
--- src/streamlit/pages/Clustering.py
+++ src/streamlit/pages/Clustering.py
@@ -27,6 +27,14 @@ def lire_css(fichier):
     with open(fichier, 'r', encoding='utf-8') as f:
         return f.read()
 
+# Retourne le ou les genre des film dans une liste.
+# df_row correspond à une serie réprésentant une seule ligne de dataset
+def donne_genre_film(sr_row) :
+    liste_g = [col for i, col in enumerate(sr_row.index) if sr_row.index.str.startswith('GE')[i]]
+    valeur_g = [genre.replace('GE_', '') for genre in liste_g if sr_row.loc[genre, '0'] == 1]
+    return valeur_g
+
+
 # Lire le fichier de styles perso
 contenu_html = lire_css('pages/st.css')
 
@@ -74,25 +82,32 @@ if user_input:
 
         # Prédiction du cluster du film sélectionné
         selected_movie_cluster = kmeans.predict(movie_data_normalized)
-
+        
         # Filtrage des films du même cluster
         cluster_movies = df[df['cluster'] == selected_movie_cluster[0]]
 
-        cluster_movies = cluster_movies[cluster_movies['TI_primaryTitle'] != selected_movie_info.iloc[0]['TI_primaryTitle']]
         cluster_movies = cluster_movies[cluster_movies['TI_primaryTitle'] != selected_movie_info.iloc[0]['TI_primaryTitle']]
 
         # Sélection des films recommandés
         recommended_movies = cluster_movies.loc[:, ['TI_primaryTitle', 'TI_startYear', 'TI_budget', 'TI_revenue', 
                                                     'RA_averageRating', 'TI_runtimeMinutes', 'TI_overview', 'RA_numVotes', 
-                                                    'TI_region', 'TI_language','TI_poster_path']].head(5)
+                                                    'TI_region', 'TI_language','TI_poster_path',
+                                                    'GE_Action', 'GE_Adult', 'GE_Adventure', 'GE_Animation', 'GE_Biography',
+                                                    'GE_Comedy', 'GE_Crime', 'GE_Documentary', 'GE_Drama', 'GE_Family',
+                                                    'GE_Fantasy', 'GE_Film-Noir', 'GE_History', 'GE_Horror', 'GE_Music',
+                                                    'GE_Musical', 'GE_Mystery', 'GE_Romance', 'GE_Sci-Fi', 'GE_Sport',
+                                                    'GE_Thriller', 'GE_War', 'GE_Western']].head(5)
         recommended_movies['TI_poster_path'].fillna('http://www.clipartbest.com/cliparts/yio/66g/yio66g5eT.jpg',inplace=True)
-        
+
         # Affichage des informations du film sélectionné
         col1.header("Film sélectionné :")
         
         col11, col12 = col1.columns([1, 2])
 
         col11.image(selected_movie_info.iloc[0]['TI_poster_path'], width=200, use_column_width=True)
+        # ajout du/des genres
+        # selected_movie_info.iloc[0]
+        # liste_genre = ', '.join(donne_genre_film(selected_movie_info.iloc[0]))
 
         detail_film = f'''
                 **Titre :** {selected_movie_info.iloc[0]['TI_primaryTitle']} \n
@@ -117,11 +132,12 @@ if user_input:
             average_rating = row['RA_averageRating']
             num_votes = round(row['RA_numVotes'])
 
-
             with tab1_reco[index]:
                 col21, col22 = tab1_reco[index].columns([1, 2])
                 col21.image(poster_path, width=400, use_column_width=True)
-
+                # row
+                # ajout du/des genres
+                # liste_genre = ', '.join(donne_genre_film(row.to_frame()))
                 detail_film_reco = f'''
                         **Titre :** {movie_title} \n
                         **Date de sortie :** {start_year} \n
